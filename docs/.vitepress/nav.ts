19240aa8920c97f29424de00393575ac10f74920
Linag <17639884250@139.com>
2024-08-29 16:05:53 +0800

    first commit

---
+++ docs/.vitepress/nav.ts
@@ -0,0 +1,18 @@
+export const nav = [
+  { text: '主页', link: '/index' },
+  { text: '计算机基础', link: '/计算机基础/1.计算机基础' },
+  { text: '数据结构', link: '/计算机基础/1.计算机基础' },
+  {
+    text: '前端基础',
+    items: [
+      { text: 'HTML', link: '/前端基础/1.html.md' },
+      { text: 'CSS', link: '/前端基础/2.css.md' },
+    ],
+  },
+  { text: 'JavaScript', link: '/计算机基础/1.计算机基础' },
+  { text: 'TypeScript', link: '/计算机基础/1.计算机基础' },
+  { text: 'Vue', link: '/计算机基础/1.计算机基础' },
+  { text: 'React', link: '/计算机基础/1.计算机基础' },
+  { text: 'vite', link: '/计算机基础/1.计算机基础' },
+  { text: 'webpack', link: '/计算机基础/1.计算机基础' },
+]
